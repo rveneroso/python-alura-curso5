354b59e28ddbc305bfa53bc06fe4bcdd1b27c4e7
Renato Veneroso <rveneroso@gmail.com>
2022-01-29 17:39:05 -0300

    Primeiro commit do quinto curso de Python.

---
+++ main.py
@@ -0,0 +1,8 @@
+url = "http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
+print(url)
+
+url_base = url[0:26]
+print(url_base)
+
+url_parametros = url[27:81]
+print(url_parametros)
