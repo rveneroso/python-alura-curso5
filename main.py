845397b20d5d4696b54b8e915454370884847b89
Renato Veneroso <rveneroso@gmail.com>
2022-01-29 17:58:58 -0300

    Uso do método find() para identificar a posição onde a interrogação se encontra. Fatiamento com omissão das posições inicial e final.

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 url = "http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
 print(url)
 
-url_base = url[0:26]
+#
+# Usando o método find de str para identificar a posição onde determinado caracter se encontra
+#
+indice_interrogacao = url.find('?')
+
+#
+# No fatiamento de Strings, quando o primeiro argumento é omitido, a String retornada irá conter
+# a String original desde o início até a posição definida.
+#
+url_base = url[:indice_interrogacao] # Do início da String até a posição anterior à interrogação
 print(url_base)
 
-url_parametros = url[27:81]
+#
+# No fatiamento de Strings, quando o segundo argumento é omitido, a String retornada irá conter
+# a String original da posição definida até o seu fim.
+#
+url_parametros = url[indice_interrogacao+1:] # Da posição posterior à interroação até o fim.
 print(url_parametros)
