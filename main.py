e645d5358bd3494f847020b322abafc4ea7c423e
Renato Veneroso <rveneroso@gmail.com>
2022-01-29 19:21:39 -0300

    Alterações feitas por mim conta: criada a classe Util, um método para retornar o valor de um dado parâmetro da URL passada ao método e criação de uma função pesquisar() no arquivo main.py. Provavelmente tudo isto será desfeito ao fim desse módulo do curso pois devo pegar a versão disponibilizada pelo instruções e sobreescrever meu arquivo.

--- main.py
+++ main.py
@@ -1,38 +1,51 @@
-#
-# Formato de URL utilizado na aula que apresenta o método len()
-#
-url = "http://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
-print(url)
+class Util:
+    def retorna_valor_parametro(nome_parametro, url_parametros):
+        indice_parametro = url_parametros.find(nome_parametro)
+        indice_valor = indice_parametro + len(nome_parametro) + 1
+        indice_e_comercial = url_parametros.find('&', indice_valor)
+        if indice_e_comercial == -1:
+             valor = url_parametros[indice_valor:]
+        else:
+             valor = url_parametros[indice_valor:indice_e_comercial]
+        return valor
 
-#
-# Usando o método find de str para identificar a posição onde determinado caracter se encontra
-#
-indice_interrogacao = url.find('?')
+from main import Util
 
-#
-# Somente faz a extração das substrings caso a ? tenha sido encontrada na URL.
-#
-if(indice_interrogacao>0):
+def pesquisar():
     #
-    # No fatiamento de Strings, quando o primeiro argumento é omitido, a String retornada irá conter
-    # a String original desde o início até a posição definida.
+    # Formato de URL utilizado na aula que apresenta o método len()
     #
-    url_base = url[:indice_interrogacao] # Do início da String até a posição anterior à interrogação
+    url = "http://bytebank.com/cambio?quantidade=100&moedaDestino=dolar&moedaOrigem=real"
 
     #
-    # No fatiamento de Strings, quando o segundo argumento é omitido, a String retornada irá conter
-    # a String original da posição definida até o seu fim.
+    # Usando o método find de str para identificar a posição onde determinado caracter se encontra
     #
-    url_parametros = url[indice_interrogacao+1:] # Da posição posterior à interroação até o fim.
-    print(url_parametros)
+    indice_interrogacao = url.find('?')
 
     #
-    # Procura pelo parâmetro moedaOrigem
+    # Somente faz a extração das substrings caso a ? tenha sido encontrada na URL.
     #
-    parametro_busca = 'moedaOrigem'
-    indice_moeda_origem = url.find(parametro_busca)
-    if(indice_moeda_origem>0):
+    if(indice_interrogacao>0):
         #
-        # Essa forma de busca só funciona com a URL estando no formato definido no início do programa.
-        moeda_origem = url[indice_moeda_origem + len(parametro_busca) + 1:]
-        print(f'A conversão será feita para a moeda {moeda_origem}')
+        # No fatiamento de Strings, quando o primeiro argumento é omitido, a String retornada irá conter
+        # a String original desde o início até a posição definida.
+        #
+        url_base = url[:indice_interrogacao] # Do início da String até a posição anterior à interrogação
+
+        #
+        # No fatiamento de Strings, quando o segundo argumento é omitido, a String retornada irá conter
+        # a String original da posição definida até o seu fim.
+        #
+        url_parametros = url[indice_interrogacao+1:] # Da posição posterior à interroação até o fim.
+
+        #
+        # Busca o valor de um parâmetro
+        #
+        parametros = ['quantidade', 'moedaOrigem', 'moedaDestino']
+        for parametro in parametros:
+            valor_parametro = Util.retorna_valor_parametro(parametro, url_parametros)
+            print(f'O valor do parâmetro {parametro} na URL é {valor_parametro}')
+
+
+if(__name__ == "__main__"):
+    pesquisar()
