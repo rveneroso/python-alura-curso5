1999e1e83aff3ffe1d07371cdb0133dc5390994a
Renato Veneroso <rveneroso@gmail.com>
2022-01-30 11:41:20 -0300

    Implementação do método str() na classe ExtratorURL.

--- extrator_url.py
+++ extrator_url.py
@@ -52,9 +52,15 @@ class ExtratorURL:
     def __len__(self):
         return len(self.url)
 
+    #
+    # Sobrescrita do método str
+    #
+    def __str__(self):
+        return self.url + "\nParâmetros: " + self.get_url_parametros() + "\n" + "URL Base: " + self.get_url_base()
 
 url = "http://bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
 extrator_url = ExtratorURL(url)
 valor_quantidade = extrator_url.get_valor_parametro("quantidade")
 print(valor_quantidade)
 print(f'O tamanho da URL é {len(extrator_url)}')
+print(extrator_url)
